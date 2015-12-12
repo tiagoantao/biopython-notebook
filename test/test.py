fa40aae8bd0ffd91eb0e82f44e93467f9667a1cb
Tiago Antao <tiagoantao@gmail.com>
2015-12-12 13:26:53 -0700

    Notebook testing

---
+++ test/test.py
@@ -0,0 +1,33 @@
+import os
+
+from nbconvert.preprocessors.execute import ExecutePreprocessor
+from nbconvert.nbconvertapp import NbConvertApp
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors.execute import CellExecutionError
+import nbformat
+
+folder = 'notebooks'
+os.chdir(folder)
+nblist = ['01 - Introduction.ipynb',
+          '02 - Quick Start.ipynb',
+          '03 - Sequence Objects.ipynb',
+          '04 - Sequence Annotation objects.ipynb',
+          '05 - Sequence Input and Output.ipynb',
+          '12 - Bio.PopGen - Population Genetics.ipynb',
+          '13 - Phylogenetics with Bio.Phylo.ipynb',
+          '17 - Graphics including GenomeDiagram.ipynb',
+          '99 - Credits.ipynb']
+
+ex = ExecutePreprocessor()
+ex.timeout = 30  # seconds
+ex.interrupt_on_timeout = True
+
+for notebook in nblist:
+    nb = nbformat.read(notebook, as_version=4)
+    try:
+        nb_executed, resources = ex.preprocess(nb, resources={})
+        print('Pass: ' + notebook)
+    except CellExecutionError as e:
+        print('Fail: '  + notebook + ': ' + e.traceback[-1])
+
+os.chdir('..')
